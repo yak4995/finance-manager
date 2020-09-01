168887ffdf202143ab4470a88f59ece925ab43f9
Yuriy Kryvokhyzha <yak4995@gmail.com>
2020-09-01 23:06:12 +0300

    added forgotten files

---
+++ src/infrastructure/ui/transactionCategories/events/transactionCategoryShouldBeDeleted.event.ts
@@ -0,0 +1,17 @@
+import IEvent from '../../../../core/app/events/event.interface';
+import { EventStatus } from '../../../../core/app/events/eventStatus.enum';
+import ITransactionCategory from '../../../../core/domain/transactions/entities/transactionCategory.interface';
+
+export default class TransactionCategoryShouldBeDeletedEvent implements IEvent {
+  private eventState: EventStatus = EventStatus.WAITING;
+
+  constructor(public readonly category: ITransactionCategory) {}
+
+  get state(): EventStatus {
+    return this.eventState;
+  }
+
+  set state(state: EventStatus) {
+    this.eventState = state;
+  }
+}
---
+++ src/infrastructure/ui/transactionCategories/listeners/transactionCategoryShouldBeDeleted.listener.ts
@@ -0,0 +1,27 @@
+import IEventListener from '../../../../core/app/events/eventListener.interface';
+import IRepository from '../../../../core/domain/repository.interface';
+import TransactionCategoryShouldBeDeletedEvent from '../events/transactionCategoryShouldBeDeleted.event';
+import ITransactionCategory from '../../../../core/domain/transactions/entities/transactionCategory.interface';
+import TransactionCategoryAbstractFactory from '../../../../core/domain/transactions/factories/transactionCategoryFactory';
+
+export default class TransactionCategoryShouldBeDeletedListener
+  implements IEventListener<TransactionCategoryShouldBeDeletedEvent> {
+  private readonly transactionCategoryRepo: IRepository<ITransactionCategory>;
+  constructor(
+    private readonly transactionCategoryFactory: TransactionCategoryAbstractFactory,
+  ) {
+    this.transactionCategoryRepo = transactionCategoryFactory.createTransactionCategoryRepo();
+  }
+
+  // TODO: magic literals to configs/enum with templates
+  async process(
+    event: TransactionCategoryShouldBeDeletedEvent,
+  ): Promise<ITransactionCategory> {
+    const result: ITransactionCategory[] = await this.transactionCategoryRepo.delete(
+      {
+        id: event.category.id,
+      },
+    );
+    return result[0];
+  }
+}
